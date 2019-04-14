c18f8dbd6763c1b4e8c48a65738dbfab7dfd5ade
sabelogumede <iamsabelogumede@gmail.com>
2019-04-14 21:16:56 +0200

    added Singleton and Factory pattern

--- index.js
+++ index.js
@@ -24,25 +24,74 @@
 // console.log(civic);
 
 // ######## 03 Constructor-Pattern 
+// class Car {
+//     constructor(doors, engine, color) {
+//         this.doors = doors;
+//         this.engine = engine;
+//         this.color = color;
+//     }
+// }
+
+// // externds-the-car-class
+// class Suv extends Car {
+//     constructor(doors, engine, color) {
+//         super(doors, engine, color);
+//         this.wheels = 4;
+//     }
+// }
+
+// const civic = new Car(4, 'V6', 'grey');
+// const cx5 = new Suv(4, "V8", "red");
+
+// console.log(civic);
+// console.log(cx5);
+
+// ######## 04 Singleton-Pattern 
+// let instance = null;
+
+// class Car {
+//     constructor(doors, engine, color) {
+//         if (!instance) {
+//             this.doors = doors;
+//             this.engine = engine;
+//             this.color = color;
+//             instance = this;
+//         } else {
+//             return instance;
+//         }
+
+//     }
+// }
+
+// const civic = new Car(4, 'V6', 'grey');
+// const honda = new Car(2, 'V4', 'red');
+
+// console.log(civic);
+// console.log(honda);
+
+// ######## 05 Factory-Pattern 
 class Car {
     constructor(doors, engine, color) {
         this.doors = doors;
         this.engine = engine;
         this.color = color;
     }
+
 }
 
-// externds-the-car-class
-class Suv extends Car {
-    constructor(doors, engine, color) {
-        super(doors, engine, color);
-        this.wheels = 4;
+class carFactory {
+    createCar(type) {
+        switch(type) {
+            case 'civic':
+                return new Car(4, 'V6', 'grey')
+            case 'honda':
+                return new Car(2, 'V4', 'red')
+        }
     }
 }
 
-const civic = new Car(4, 'V6', 'grey');
-const cx5 = new Suv(4, "V8", "red");
+const factory = new carFactory();
+const honda = factory.createCar('honda');
 
-console.log(civic);
-console.log(cx5);
+console.log(honda);
 
